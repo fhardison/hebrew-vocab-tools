1b6498473f51c6176e78a04fbc287cb7f9d60d97
fhardison <fhardison@gmail.com>
2021-10-05 08:44:50 -0400

    Updated examples.py

--- examples.py
+++ examples.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from collections import Counter
-from main import TokenType, ChunkType, get_tokens_by_chunk
+from main import TokenType, ChunkType, get_tokens_by_chunk, get_tokens
 from heb_lex_tools import HEBLEX
 
 # Count lemmas in a book
@@ -9,8 +9,7 @@ def count_lemmas(book_id):
     return len(get_tokens_by_chunk(TokenType.lemma, ChunkType.book)[book_id])
 
 
-def build_flashcard_spreadsheet(book_id, limit, output_name, lower_limit=0):
-    glosser = HEBLEX()
+def build_flashcard_spreadsheet(book_id, limit, output_name, glosser, lower_limit=0):
     lemmas = Counter(get_tokens_by_chunk(TokenType.lemma, ChunkType.book)[book_id])
     delim = '\t'
     total_items = 0
@@ -24,8 +23,26 @@ def build_flashcard_spreadsheet(book_id, limit, output_name, lower_limit=0):
         print(f"Total items in {book_id} below {limit} was {total_items}", file=f)
 
 
+GLOSSER = HEBLEX()
+HEB_LEMMAS = Counter(get_tokens(TokenType.lemma))
+
+def get_hapax_in_book(book_id, total_counts, glosser):
+    book_counts = Counter(get_tokens_by_chunk(TokenType.lemma, ChunkType.book)[book_id])
+    with open(f"{book_id}_hapax.tab", 'w', encoding="UTF-8") as f:
+        print('\t'.join(["Lemma", "Count in book", "Count in Hebrew Bible", "Gloss"]), file=f)
+        for (lemma, count) in book_counts.most_common():
+            if count == 1:
+                print('\t'.join([glosser.strongs_to_lemma(lemma),
+                                 str(count),
+                                 str(total_counts[lemma]),
+                                 glosser.strongs_to_gloss(lemma)]), file=f)
+
+
 # build flachard spreadsheet including hapaxlegomena
-build_flashcard_spreadsheet("Isa", 100, "Isa_vocab.tab")
+build_flashcard_spreadsheet("Isa", 100, "Isa_vocab.tab", GLOSSER)
 
 # buildi flashcard spreadsheet without hapaxlegomena.
-build_flashcard_spreadsheet("Isa", 100, "Isa_vocab_no_hapax.tab", 2)
+build_flashcard_spreadsheet("Isa", 100, "Isa_vocab_no_hapax.tab", GLOSSER, 2)
+
+# Get data on hapaxlegomena in book
+get_hapax_in_book("Isa", HEB_LEMMAS, GLOSSER)
